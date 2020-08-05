b2dcbf75d02168beba6e24fd373b8ee388f6d158
sylvia <ojitoscecy.com>
2020-08-05 14:43:14 -0500

    add employee

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,9 +1,18 @@
-const mysql = require('mysql')
-const pool = require('../sql/connection')
-const { handleSQLError } = require('../sql/error')
+const mysql = require('mysql');
+const pool = require('../sql/connection');
+const { handleSQLError } = require('../sql/error');
+
+
+const getEmployees =(req, res) =>{
+    pool.query("SELECT * FROM employees LIMIT 50", (err, rows)=> {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+});
+};
 
 
 
 module.exports = {
+    getEmployees
 
 }
--- routers/employees.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const employeesController = require('../controllers/employees')
-const router = express.Router()
-
-
-
-
-
-module.exports = router
