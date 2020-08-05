a51f092d7a341bf72c74189ec8e03231241f360c
monica-suarez <shutterbugmo@gmail.com>
2020-08-05 15:27:08 -0500

    added delete

--- controllers/employees.js
+++ controllers/employees.js
@@ -2,6 +2,8 @@ const mysql = require('mysql');
 const pool = require('../sql/connection');
 const { handleSQLError } = require('../sql/error');
 
+//id refers to employee number in DB
+
 
 const getAllEmployees =(req, res) =>{
     pool.query("SELECT * FROM employees LIMIT 50", (err, rows)=> {
@@ -24,7 +26,7 @@ const getEmployeeByFirstName = (req, res) =>{
 
 
 const getEmployeeById = (req, res) => {
-  const id = req.params.id;
+  const id = req.params.emp_no;
   let sql = `SELECT * FROM ?? WHERE ?? = ?`;
   sql = mysql.format(sql, ['*','employees', 'emp_no', id]);
   
@@ -46,12 +48,12 @@ const createNewEmployee = (req, res) =>{
 
     pool.query(sql, (err, results) => {
         if (err) return handleSQLError(res, err)
-        return res.json({ newId: results.insertId });
+        return res.json({ newEmployee: results.emp_no });
       })
 }
 
 const updateEmployeeById = (req, res) => {
-    const id = req.params.id
+    const id = req.params.emp_no
     let updatedEmployee = req.body
     const birth_date = updatedEmployee.birth_date
     const first_name = updatedEmployee.first_name 
@@ -59,19 +61,29 @@ const updateEmployeeById = (req, res) => {
     const gender = updatedEmployee.gender
     const hire_date = updatedEmployee.hire_date
     let sql = "UPDATE ?? SET ?? = ?, ?? = ?, ?? = ?, ?? = ?, ?? = ? WHERE ?? = ?"
-    sql = mysql.format(sql, ['employees', 'birth_date', birth_date, 'first_name', first_name, 'last_name', last_name, 'gender', gender, 'hire_date', hire_date, 'id', id])
+    sql = mysql.format(sql, ['employees', 'birth_date', birth_date, 'first_name', first_name, 'last_name', last_name, 'gender', gender, 'hire_date', hire_date, 'emp_no', id])
   
     pool.query(sql, (err, results) => {
       if (err) return handleSQLError(res, err)
-      return res.status(204).json({ message: `Updated ${results.affectedRows} user(s)` });
+      return res.status(204).json({ message: `Updated ${results.affectedRows} employee(s)` });
     })
   }
-
+const deleteEmployeebyId = (req, res) =>{
+    const id = req.params.emp_no;
+    let sql = "DELETE FROM ?? WHERE ?? = ?"
+    sql = mysql.format(sql, ['employees', 'emp_no', id])
+  
+    pool.query(sql, (err, results) => {
+      if (err) return handleSQLError(res, err)
+      return res.json({ message: `Deleted ${results.affectedRows} employee(s)` });
+    })
+}
 
 module.exports = {
     getAllEmployees,
     getEmployeeById,
     getEmployeeByFirstName,
     createNewEmployee,
-    updateEmployeeById
+    updateEmployeeById,
+    deleteEmployeebyId
 };
