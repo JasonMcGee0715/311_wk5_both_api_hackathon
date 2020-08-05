2866cec0239609b97554d42a1cec775362d149bb
monica-suarez <shutterbugmo@gmail.com>
2020-08-05 08:53:43 -0500

    changes to update function

--- routers/employees.js
+++ routers/employees.js
@@ -3,8 +3,8 @@ const employeesController = require('../controllers/employees')
 const router = express.Router()
 
 
-router.get('/:first_name)', employeesController.getEmployeeByFirstName)
+router.get('/firstname/:first_name)', employeesController.getEmployeeByFirstName)
 
-router.put(('/:first_name'), employeesController.updateEmployeeByFirstName)
+router.put(('/:id'), employeesController.updateEmployeeById)
 
 module.exports = router
